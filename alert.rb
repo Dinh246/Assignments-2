8b5e4042644485d7d26708508efe4a74a65ac8fa
DinhTruong <nickyb246@gmail.com>
2020-09-22 09:48:37 +0700

    Bai tap alert

---
+++ alert.rb
@@ -0,0 +1,14 @@
+require 'selenium-webdriver'
+require 'rubygems'
+browser = Selenium::WebDriver.for :chrome
+browser.navigate.to 'https://www.seleniumeasy.com/test/javascript-alert-box-demo.html'
+alert = browser.find_element(xpath: "//button[@class='btn btn-default']").click
+alert = browser.switch_to.alert.accept
+comfirm = browser.find_element(xpath: "//button[@class='btn btn-default btn-lg'][contains(text(),'Click me!')]").click
+comfirm = browser.switch_to.alert.dismiss
+alert_prompt = browser.find_element(xpath: "//button[contains(text(),'Click for Prompt Box')]").click
+alert_prompt = browser.switch_to.alert
+alert_prompt.send_keys('Dinh dep trai')
+alert_prompt.accept
+sleep 5
+browser.quit
